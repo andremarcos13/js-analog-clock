5933f84b6f1315149b80f0c73c0333b10a7e781f
Andre Cesar <andrmgc@gmail.com>
2022-11-10 17:37:38 -0300

    cria funcao getTime, que ira pegar segundos, minutos e horas atuais

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const sec = document.getElementById('seconds');
 const min = document.getElementById('minutes');
 const hr = document.getElementById('hours')
+
+const getTime = () => {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+}
+
